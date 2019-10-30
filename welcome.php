b8140f73c66b7028b4244d4d7881e571937de14e
LENZFLIKER <cm.manuja@gmail.com>
2019-10-30 15:41:56 +0530

    Add landing page

--- welcome.php
+++ welcome.php
@@ -1,10 +1,91 @@
-<!DOCTYPE html>
+<!--
+ =========================================================
+ * Material Kit - v2.0.6
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-kit
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+   Licensed under MIT (https://github.com/creativetimofficial/material-kit/blob/master/LICENSE.md)
+
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+
+
+ <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title> </title>
+  <title>SeatStock</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- Material Kit CSS -->
+  <link href="assets/css/material-kit.css?v=2.0.6" rel="stylesheet" />
 </head>
+
 <body>
-    loged in
+  <nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="">
+          SeatStock </a><!--
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>-->
+      </div>
+      <!--<div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a href="#register" class="btn btn-rose btn-raised btn-round " data-toggle="dropdown">
+              Register
+            <div class="ripple-container"></div></a>
+          </li>
+        </ul>
+      </div>-->
+    </div>
+  </nav>
+  <div class="page-header header-filter" data-parallax="true" style="background-image: url('http://www.newinteriorslk.com/wp-content/uploads/2018/05/NSBM-42.jpg')">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 ml-auto mr-auto">
+          <div class="brand text-center">
+            <h1>I WANT TO</h1>
+            <button class="btn btn-primary btn-round btn-lg">
+                <i class="material-icons">event_seat</i> find and reserve
+              <div class="ripple-container"></div></button>
+            <button class="btn btn-primary btn-round btn-lg">
+              <i class="material-icons">create</i> create and vend
+            <div class="ripple-container"></div></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="main main-raised">
+    <div class="container">
+      <div class="section text-center">
+        <h2 class="title">not your everyday booking site</h2>
+      </div>
+    </div>
+  </div>
+  <footer class="footer footer-default">
+    <div class="container">
+      <div class="copyright float-right">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by SeatStock
+    </div>
+  </footer>
 </body>
+
 </html>
