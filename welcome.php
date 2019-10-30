60113640b6ef81ff49504cccaec6547a12edf7a9
sanjana-s99 <s.witharanage@students.nsbm.lk>
2019-10-30 21:23:34 +0530

    logout

--- welcome.php
+++ welcome.php
@@ -42,7 +42,8 @@
           <a class="navbar-brand" href="logout.php">
           Log Out </a>
           <a class="navbar-brand">
-          Welcome <?php echo $_SESSION['username']; ?>! </a><!--
+          Welcome <?php echo $_SESSION['username']; ?>! </a>
+          <!--
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Toggle navigation</span>
           <span class="navbar-toggler-icon"></span>
