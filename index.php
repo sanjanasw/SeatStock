dc335fe65ee06c167d082c2b1cf7bac4068357ed
LENZFLIKER <cm.manuja@gmail.com>
2019-11-03 04:40:29 +0530

    Move index.html to index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./bootstrap/css/bootstrap.css">
+  <script src="./bootstrap/js/bootstrap.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/eee0ff9583.js" crossorigin="anonymous"></script>
@@ -13,55 +13,20 @@
   <title>SeatStock</title>
 </head>
 <body>
-  <?php include "includes/navbar.php"; ?>
-  <div class="container">
-    test
+  <div class="card shadow-lg p-3 mb-5 bg-white rounded-lg" style="max-width: 30rem;">
+    <div class="row text-center">
+      <div class="col">
+        <h1>I WANT TO</h1>
+      </div>
+    </div>
+    <div class="row text-center">
+      <div class="col">
+        <button type="button" class="btn btn-primary btn-lg"><a href = "events/event_index.php" class="text-white nounderline"> FIND AND BOOK</a></button>
+      </div>
+      <div class="col">
+        <button type="button" class="btn btn-primary btn-lg"><a href = "create/create_index.php" class="text-white nounderline"> MAKE AND VEND</a></button>
+      </div>
+    </div>
   </div>
 </body>
-</html>
-
-
-
-
-
-
-
-<body>
-
-  <div class="page-header header-filter" data-parallax="true" style="background-image: url('http://www.newinteriorslk.com/wp-content/uploads/2018/05/NSBM-42.jpg')">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 ml-auto mr-auto">
-          <div class="brand text-center">
-            <h1>I WANT TO</h1>
-            <a href = "events/event_index.php"><button class="btn btn-primary btn-round btn-lg">
-                <i class="material-icons">event_seat</i> find and reserve
-              <div class="ripple-container"></div></button></a>
-           <a href = "create/create_index.php"> <button class="btn btn-primary btn-round btn-lg">
-              <i class="material-icons">create</i>create and vend
-            <div class="ripple-container"></div></button></a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="main main-raised">
-    <div class="container">
-      <div class="section text-center">
-        <h2 class="title">not your everyday booking site</h2>
-      </div>
-    </div>
-  </div>
-  <footer class="footer footer-default">
-    <div class="container">
-      <div class="copyright float-right">
-        &copy;
-        <script>
-          document.write(new Date().getFullYear())
-        </script>, made with <i class="material-icons">favorite</i> by SeatStock
-    </div>
-      </div>
-  </footer>
-</body>
-
 </html>
