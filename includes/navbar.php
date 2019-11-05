9294392b7c99671a47e192cc2709d2d0e92b80b4
sanjana-s99 <s.witharanage@students.nsbm.lk>
2019-11-05 16:05:43 +0530

    nav bar

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,25 +10,9 @@
           <a class="navbar-brand" href="includes/logout.php">
               Log Out </a>
           <a class="navbar-brand" href="admin/viwe_booking.php">
-              Test1 </a>
+              View Users </a>
           <a class="navbar-brand" href="admin/view_app_u.php">
               Test2 </a>
-          <!--/
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-        </button>-->
       </div>
-      <!--<div class="collapse navbar-collapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="#register" class="btn btn-rose btn-raised btn-round " data-toggle="dropdown">
-              Register
-            <div class="ripple-container"></div></a>
-          </li>
-        </ul>
-      </div>-->
     </div>
   </nav>
