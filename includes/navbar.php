8f4fc564f7abc58755ddd269b05f220b215f21a0
sanjana-s99 <s.witharanage@students.nsbm.lk>
2019-11-06 10:26:00 +0530

    my profile added

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,22 @@
 <nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
     <div class="container">
-      <div class="navbar-translate">
-        <a class="navbar-brand" href="index.php">
-          SeatStock </a>
-          <a class="navbar-brand" href="index_reg.php">
-          Sign Up</a>
-          <a class="navbar-brand" href="index_log.php">
-          Log In </a>
-          <a class="navbar-brand" href="includes/logout.php">
-              Log Out </a>
-          <a class="navbar-brand" href="hosters/viwe_booking.php">
-              View Users </a>
-          <a class="navbar-brand" href="hosters/guest.php">
-              Guest </a>
-          <a class="navbar-brand" href="admin/view_app_u.php">
-              Test2 </a>
-      </div>
+        <div class="navbar-translate">
+            <a class="navbar-brand" href="index.php">
+                SeatStock </a>
+            <a class="navbar-brand" href="index_reg.php">
+                Sign Up</a>
+            <a class="navbar-brand" href="index_log.php">
+                Log In </a>
+            <a class="navbar-brand" href="includes/logout.php">
+                Log Out </a>
+            <a class="navbar-brand" href="hosters/viwe_booking.php">
+                View Users </a>
+            <a class="navbar-brand" href="hosters/guest.php">
+                Guest </a>
+            <a class="navbar-brand" href="profile/my_profile.php">
+                Test1 </a>
+            <a class="navbar-brand" href="admin/view_app_u.php">
+                Test2 </a>
+        </div>
     </div>
-  </nav>
+</nav>
--- profile/my_profile.php
+++ profile/my_profile.php
@@ -40,70 +40,64 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <script src="../bootstrap/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/eee0ff9583.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>SeatStock</title>
 </head>
-<body>
-   
-                       <form method="POST" action="">
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">first name</label>
-                                    <input class="input--style-4" type="text" name="user_fname" value="<?php if(isset($user_fname)){echo $user_fname;}?>">
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">last name</label>
-                                    <input class="input--style-4" type="text" name="user_lname" value="<?php if(isset($user_lname)){echo $user_lname;}?>">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Password</label>
-                                    <input class="input--style-4" type="password" name="user_password" value="<?php if(isset($user_password)){echo $user_password;}?>">
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Re type-Password</label>
-                                    <input class="input--style-4" type="password" name="." value="<?php if(isset($user_password)){echo $user_password;}?>">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Username</label>
-                                    <div class="input-group-icon">
-                                        <input class="input--style-4 " type="text" name="user_name" value="<?php if(isset($user_name)){echo $user_name;}?>">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Email</label>
-                                    <input class="input--style-4" type="email" name="user_email" value="<?php if(isset($user_email)){echo $user_email;}?>">
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Phone Number</label>
-                                    <input class="input--style-4" type="text" name="user_tp" value="<?php if(isset($user_tp)){echo $user_tp;}?>">
-                                </div>
-                            </div>
-                        </div>
 
-                        <div class="p-t-15">
-                            <input class="btn btn--radius-2 btn--blue" name="submit" type="submit" value="Update">
-                        </div>
-                    </form>
-    
+<body>
+    <div class="card shadow-lg p-3 mb-5 bg-white rounded-lg text-center" style="max-width: 30rem;">
+        <div class="container shadow-lg p-2 mb-3 bg-blue-gradient rounded-lg" style="max-width: 28rem;">
+            <h3 class="text-white">EDIT YOUR ACCOUNT DETAILS</h3>
+        </div>
+        <div class="container shadow-lg p-3 bg-redish-gradient rounded-lg" style="max-width: 28rem;">
+            <form method="POST" action="">
+                <div class="form-group">
+                    <h5 class="text-white">First Name</h5>
+                    <input class="form-control" type="text" name="user_fname" value="<?php if(isset($user_fname)){echo $user_fname;}?>">
+                </div>
+                <div class="form-group">
+                    <h5 class="text-white">Last Name</h5>
+                    <input class="form-control" type="text" name="user_lname" value="<?php if(isset($user_lname)){echo $user_lname;}?>">
+                </div>
+                <div class="form-group">
+                    <h5 class="text-white">Password</h5>
+                    <input class="form-control" type="password" name="user_password" value="<?php if(isset($user_password)){echo $user_password;}?>">
+                </div>
+                <!-- <div class="col-2">
+                <div class="input-group">
+                    <label class="label">Re type-Password</label>
+                    <input class="input--style-4" type="password" name="." value="<?php //if(isset($user_password)){echo $user_password;}?>">
+                </div>
+            </div>-->
+                <div class="form-group">
+                    <h5 class="text-white">User Name</h5>
+                    <input class="form-control" type="text" name="user_name" value="<?php if(isset($user_name)){echo $user_name;}?>">
+                </div>
+                <div class="form-group">
+                    <h5 class="text-white">Email</h5>
+                    <input class="form-control" type="email" name="user_email" value="<?php if(isset($user_email)){echo $user_email;}?>">
+                </div>
+                <div class="form-group">
+                    <h5 class="text-white">Phone Number</h5>
+                    <input class="form-control" type="text" name="user_tp" value="<?php if(isset($user_tp)){echo $user_tp;}?>">
+                </div>
+                <div class="form-group mb-1">
+                    <button class="btn btn-primary btn-link btn-wd btn-lg bg-blue-gradient text-white nounderline" style="width:100%;" name="submit" type="submit" value="Sign Up">Change
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
