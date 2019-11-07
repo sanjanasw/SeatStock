ab910e9ea57827e27f0c495ac77320dd16bed5d7
sanjana-s99 <s.witharanage@students.nsbm.lk>
2019-11-07 21:36:55 +0530

    Admin

--- admin/view_app_u.php
+++ admin/view_app_u.php
@@ -18,18 +18,30 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>SeatStock</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+    <script src="../bootstrap/js/bootstrap.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/eee0ff9583.js" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>Seatstock-Admin</title>
 </head>
 
 <body onload="checkpw()">
-    <?php include "../includes/navbar.php"; ?>
 
-    <div class="container">
-        <br><br>
-        <h1 class="text-center">Users</h1>
-        <div class="col-sx-6">
-            <table border="1">
-                <tr>
+    <div class="card shadow-lg p-3 mb-5 bg-white rounded-lg text-center" style="max-width: 70rem;">
+       
+        <button class="btn btn-primary p-3 mb-3  btn-link btn-wd btn-lg bg-redish-gradient btn-outline-danger text-white nounderline" style="width:100%;"  onclick="home()"><h1>Admin Panel</h1></button>
+        
+        <div class="container shadow-lg p-2 mb-3 bg-blue-gradient rounded-lg" style="max-width: 68rem;">
+            <h1 class="text-center text-white">Users</h1>
+        </div>
+        <div class="container shadow-lg p-3 mb-3 bg-redish-gradient rounded-lg" style="max-width: 68rem;">
+           <center>
+            <table class="table-danger table-hover">
+                <tr class="table-warning">
                     <th>User Id</th>
                     <th>User Name</th>
                     <th>First Name</th>
@@ -58,20 +70,23 @@
              echo "<td>{$u6}</td>";
              echo "<td>{$u7}</td>";
              echo "<td>{$u8}</td>";
-             echo"<td><a href='approve.php?approve=$u0'>Approve</a></td>";
-			 echo"<td><a href='approve.php?d1=$u0'>!Host</a></td>";
-             echo"<td><a href='approve.php?d2=$u0'>Client</a></td>";
-             echo"<td><a href='approve.php?delete=$u0'>Delete</a></td>";
+             echo"<td><a class='btn btn-success btn-sm' href='approve.php?approve=$u0'>Approve</a></td>";
+			 echo"<td><a class='btn btn-warning btn-sm' href='approve.php?d1=$u0'>!Host</a></td>";
+             echo"<td><a class='btn btn-dark btn-sm' href='approve.php?d2=$u0'>Client</a></td>";
+             echo"<td><a class='btn btn-danger btn-sm' href='approve.php?delete=$u0'>Delete</a></td>";
         echo "</tr>";
     }
     ?>
             </table>
+            </center>
         </div>
-        <br><br>
-        <h1 class="text-center">Events</h1>
-        <div class="col-sx-6">
-            <table border="1">
-                <tr>
+        <div class="container shadow-lg p-2 mb-3 bg-blue-gradient rounded-lg" style="max-width: 68rem;">
+            <h1 class="text-center text-white">Events</h1>
+        </div>
+        <div class="container shadow-lg p-3 bg-redish-gradient rounded-lg" style="max-width: 68rem;">
+           <center>
+            <table class="table-danger table-hover">
+                <tr class='table-warning'>
                     <th>Event Id</th>
                     <th>Event Title</th>
                     <th>Hosters User Id</th>
@@ -91,16 +106,17 @@
              echo "<td>{$u3}</td>";
              echo "<td>{$u5}</td>";
              echo "<td>{$u6}</td>";
-             echo"<td><a href='approve.php?delete_e=$u1'>Delete</a></td>";
+             echo"<td><a class='btn btn-danger btn-sm' href='approve.php?delete_e=$u1'>Delete</a></td>";
         echo "</tr>";
     }
     ?>
             </table>
+            </center>
         </div>
     </div>
     <script>
         function checkpw() {
-            var a = prompt("Hi Admin" , "Enter Password : ");
+            var a = prompt("Hi Admin", "Enter Password : ");
             var b = "password";
             if (a != b) {
                 alert("Invalid Password.");
@@ -108,6 +124,10 @@
             } else {
                 alert("Welcome Team SEATSTOCK.");
             }
+        }
+
+        function home() {
+            window.location = "../index.php";
         }
 
     </script>
